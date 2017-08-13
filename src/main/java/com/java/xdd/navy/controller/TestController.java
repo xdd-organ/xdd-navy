6e81ca400212e4457cacb55760df329f1e7c7acb
qq767403510 <767403510@qq.com>
2017-08-13 18:39:58 +0800

    bootstrap的一个时间插件使用与 一些常用的icon使用

--- src/main/java/com/java/xdd/navy/controller/TestController.java
+++ src/main/java/com/java/xdd/navy/controller/TestController.java
@@ -5,13 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
     @RequestMapping("/test")
     @ResponseBody
     public Boolean test(@RequestParam("itemId") String itemId){
         System.out.println(itemId);
         return true;
+    }
+
+    @RequestMapping("/test2")
+    @ResponseBody
+    public List<String> test2(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("2017-08-21");
+        list.add("2017-08-22");
+        list.add("2017-08-23");
+        return list;
     }
 }
